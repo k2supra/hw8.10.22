231cb957324439fe9bceafbbe68dba7a7ac594ba
andrii <andrijkorolevic@gmail.com>
2022-10-08 11:43:26 +0300

    Created new task

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("shghgtg")
+try:
+    sign = input('sign ->')
+    lenght = int(input('lenght ->'))
+    trigger = input('trigger vertical = v / horizontal = h')
+    for item in range(0, lenght):
+        if trigger == 'v':
+            print(sign)
+        elif trigger == 'h':
+            print(sign, end='')
+        else:
+            raise Exception('Incorrect')
+
+except Exception as ex:
+    print(f'Error', {ex})
