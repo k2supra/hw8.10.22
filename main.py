026ebb4da2d26a6441cd2d89992bd42258049cab
andrii <andrijkorolevic@gmail.com>
2022-10-08 12:26:36 +0300

    Created new task 3

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("shghgtg")
+try:
+    sum = 0
+    while True:
+        print("Введіть число")
+        num = int(input("->"))
+        if num != 7:
+            if num < 0:
+                print("Number is negative")
+            if num > 0:
+                print("Number is positive")
+            if num == 0:
+                print("Number is equal to zero")
+        else:
+            print("Good bye!")
+            break
+
+
+
+except Exception as ex:
+    print(f'Error', {ex})
